5ee1cf70f4b69e3b71ab92f1de014516e8c9da08
Arthur Langereis <puurtuur@me.com>
2018-04-22 14:55:56 +0200

    just the board, viewed at proper angle

--- src/ld41.ts
+++ src/ld41.ts
@@ -1,4 +1,4 @@
-// Untitled - a game for LD41: THEME
+// Pincraft - a game for LD41: Combine 2 Unlikely genres
 // (c) 2018 by Arthur Langereis — @zenmumbler
 
 /// <reference path="./imports.ts" />
@@ -12,10 +12,10 @@
 class MainScene implements sd.SceneDelegate {
 	scene!: sd.Scene;
 	gameState!: GameState;
-	player!: PlayerController;
 	sound!: Sound;
 	ux: Interactable[] = [];
 	framers: Updateable[] = [];
+	board!: EntityInfo;
 	end = false;
 
 	willLoadAssets() {
@@ -28,21 +28,6 @@ class MainScene implements sd.SceneDelegate {
 
 	finishedLoadingAssets() {
 		dom.hide(".overlay.loading");
-	}
-
-	keyboardStuff() {
-		dom.on(dom.$(`input[type="radio"][name="keymap"]`), "click", evt => {
-			const radio = evt.target as HTMLInputElement;
-			if (radio.checked) {
-				const km = radio.dataset.km;
-				if (km === "qwerty") {
-					this.player.keyboardType = KeyboardType.QWERTY;
-				}
-				else {
-					this.player.keyboardType = KeyboardType.AZERTY;
-				}
-			}
-		});
 	}
 
 	gameStateChanged(_gs: GameState) {
@@ -68,15 +53,16 @@ class MainScene implements sd.SceneDelegate {
 		scene.camera.perspective(60, 0.1, 100);
 
 		// --------- LEVEL GEOMETRY
-		const levelModel = cache("model", "level");
+		const levelModel = cache("model", "board");
 		const levelShape = physics.makeShape({
 			type: physics.PhysicsShapeType.Mesh,
 			geom: levelModel.geom
 		})!;
 
-		makeEntity(scene, {
+		this.board = makeEntity(scene, {
 			transform: {
-				position: [0, -4, 0]
+				position: [0, 0, 0],
+				rotation: quat.fromEuler(0, 0, math.deg2rad(-5))
 			},
 			geom: levelModel.geom,
 			renderer: {
@@ -89,16 +75,39 @@ class MainScene implements sd.SceneDelegate {
 			}
 		});
 
+		makeEntity(scene, {
+			transform: {
+				position: [0, 0, 0],
+				rotation: quat.fromEuler(math.deg2rad(45), 0, math.deg2rad(45))
+			},
+			light: {
+				type: entity.LightType.Directional,
+				colour: [1, 1, 1],
+				intensity: .9
+			}
+		});
+		makeEntity(scene, {
+			transform: {
+				position: [0, 0, 0],
+				rotation: quat.fromEuler(math.deg2rad(-45), 0, math.deg2rad(-45))
+			},
+			light: {
+				type: entity.LightType.Directional,
+				colour: [1, 1, 1],
+				intensity: .9
+			}
+		});
+
 		// ----- PLAYER
-		this.player = new PlayerController(dom.$1("canvas"), [0, 1.1, 0], scene, this.sound);
-		this.gameState.listen(this.player);
+		// this.player = new PlayerController(dom.$1("canvas"), [0, 1.1, 0], scene, this.sound);
+		// this.gameState.listen(this.player);
 
 		// ----- Interactables
-		this.ux.push(new InfoSphere(this.gameState, scene, cache, [26, 0, 17.2], `"The well of despair"\nI can't see or hear anything in it.`));
-		this.ux.push(new InfoSphere(this.gameState, scene, cache, [-26, 0, 17.2], `"The pit of decay"\nLooking into it is making me dizzy.`));
-		this.ux.push(new HintBox(this.gameState, scene, cache, "grid"));
-		this.ux.push(new HintBox(this.gameState, scene, cache, "ring"));
-		this.ux.push(new HintBox(this.gameState, scene, cache, "num"));
+		// this.ux.push(new InfoSphere(this.gameState, scene, cache, [26, 0, 17.2], `"The well of despair"\nI can't see or hear anything in it.`));
+		// this.ux.push(new InfoSphere(this.gameState, scene, cache, [-26, 0, 17.2], `"The pit of decay"\nLooking into it is making me dizzy.`));
+		// this.ux.push(new HintBox(this.gameState, scene, cache, "grid"));
+		// this.ux.push(new HintBox(this.gameState, scene, cache, "ring"));
+		// this.ux.push(new HintBox(this.gameState, scene, cache, "num"));
 
 		for (const ia of this.ux) {
 			if (isUpdateable(ia)) {
@@ -109,67 +118,17 @@ class MainScene implements sd.SceneDelegate {
 		// ----- finish up
 		allocGeoms(scene);
 
-		dom.show("div.titles");
-		this.keyboardStuff();
-		this.sound.startMusic(false);
+		// this.sound.startMusic(false);
 	}
 
 	begin() {
-		dom.hide("div.titles");
-		this.player.go();
-
-		setTimeout(() => {
-			this.gameState.showMessage("After many years, I have found this forsaken place.\nThe orbs lie within, I... will seize their power!");
-		}, 1500);
-
-		setTimeout(() => {
-			const wasd = this.player.keyboardType === KeyboardType.QWERTY ? "WASD" : "ZQSD";
-			this.gameState.showMessage(`${wasd} to move, E to interact`);
-		}, 7500);
 	}
-
-	private inFocus: entity.Entity = 0;
 
 	update(timeStep: number) {
 		const scene = this.scene;
-		const player = this.player;
-		player.step(timeStep);
-		this.scene.camera.lookAt(player.view.pos, player.view.focusPos, player.view.up);
+		scene.camera.lookAt([0, .83, .15], [0, 0, .4], [0, 0, 1]);
 
-		// look at / interact with objects
-		const ray = vec3.sub([], player.view.focusPos, player.view.pos);
-		const arb = scene.physicsWorld.rayCastClosest(player.view.pos, ray, 1.5); // meters of reach for look/interact
-		const prevFocus = this.inFocus;
-		if (arb) {
-			const ent = scene.colliders.identifyEntity(arb.collisionObject);
-			if (control.keyboard.pressed(control.Key.E)) {
-				for (const ia of this.ux) {
-					if (ia.interact(ent)) {
-						break;
-					}
-				}
-			}
-			else {
-				if (this.inFocus !== ent) {
-					for (const ia of this.ux) {
-						if (ia.hover(ent)) {
-							break;
-						}
-					}
-				}
-			}
-			this.inFocus = ent;
-		}
-		else {
-			this.inFocus = 0;
-		}
-		if (prevFocus && prevFocus !== this.inFocus) {
-			for (const ia of this.ux) {
-				if (ia.blur(prevFocus)) {
-					break;
-				}
-			}
-		}
+		// scene.transforms.rotateByAngles(this.board.transform, [0, math.deg2rad(.1), 0]);
 
 		// send update event to those interested
 		for (const ua of this.framers) {
@@ -182,8 +141,8 @@ class MainScene implements sd.SceneDelegate {
 window.addEventListener("load", () => {
 	sd.App.initialize({
 		root: dom.$1(".stageholder"),
-		width: 1024,
-		height: 576,
+		width: 512,
+		height: 768,
 	}).then(() => {
 		io.loadFile("data/assets-ld41.json", { tryBreakCache: true, responseType: io.FileLoadType.JSON })
 		.then((assetsJSON: any) => {
